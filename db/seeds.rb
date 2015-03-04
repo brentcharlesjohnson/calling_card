1ae2c7fc8328409ecd3a445e63189ac8cc439664
Brent C. Johnson <brentcharlesjohnson@gmail.com>
2015-03-03 18:04:11 -0800

    updated Gemfile with faker and seeded db

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,21 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create!(name:  "Example User",
+						 email: "example@railstutorial.org",
+						 age:										20,
+						 hometown:							"anytown USA",
+						 password:              "foobar",
+						 password_confirmation: "foobar")
+
+cities = %w(Dallas Charlotte Los\ Angeles San\ Francisco Seattle New\ York)
+99.times do |n|
+	name  = Faker::Name.name
+	email = "example-#{n+1}@railstutorial.org"
+	age = 16 + rand(25)
+	hometown = cities.sample
+	password = "password"
+	User.create!(name:  name,
+							 email: email,
+							 age:										age,
+							 hometown:							hometown,
+							 password:              password,
+							 password_confirmation: password)
+end
