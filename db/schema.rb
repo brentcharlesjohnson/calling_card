2e23bc052570e8c9a57c9ccdfe169cf952f902ac
Brent C. Johnson <brentcharlesjohnson@gmail.com>
2015-04-07 19:21:09 -0700

    generated migrations to remove age and add birthday

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150319024329) do
+ActiveRecord::Schema.define(version: 20150408021408) do
 
   create_table "microposts", force: :cascade do |t|
     t.text     "content"
@@ -27,11 +27,11 @@ ActiveRecord::Schema.define(version: 20150319024329) do
     t.string   "name"
     t.string   "email"
     t.string   "hometown"
-    t.integer  "age"
     t.datetime "created_at",                      null: false
     t.datetime "updated_at",                      null: false
     t.string   "password_digest"
     t.boolean  "admin",           default: false
+    t.date     "birthday"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
