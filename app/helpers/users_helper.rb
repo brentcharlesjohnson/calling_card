1eeffdc0f90a0ccfc1ad8a4bc90be07018dc5c2e
Brent C. Johnson <brentcharlesjohnson@gmail.com>
2015-04-07 21:50:20 -0700

    updated users helper with an age method

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -6,4 +6,9 @@ module UsersHelper
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
     image_tag(gravatar_url, alt: user.name, class: "gravatar")
   end
+
+	def age(dob)
+		now = Time.now.utc.to_date
+		now.year - dob.year - ((now.month > dob.month || (now.month == dob.month && now.day >= dob.day)) ? 0 : 1)
+	end
 end
